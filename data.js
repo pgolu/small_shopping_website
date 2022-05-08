06064450d3c71d8bd3c9c6f10950fc18afbaff06
pgolu <104081676+pgolu@users.noreply.github.com>
2022-05-08 15:06:46 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+const data = {
+    products: [
+        {
+       id:'1',
+        name:'bike',
+        price:54000,
+        Image:'/bike 1.jpg',
+        },
+    
+
+    {
+    id:'2',
+     name:'bike',
+     price:64000,
+     Image:'/bike 2.jpg',
+    },
+     
+     
+       {
+        id:'3',
+         name:'car',
+         price:114000,
+         Image:'/car 1.jpg',
+       },
+
+    ],
+    };
+export default data;
+
+    
+    
+
+
+        
